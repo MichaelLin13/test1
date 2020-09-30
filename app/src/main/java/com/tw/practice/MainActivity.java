5e62c33056c128f33e59701bf0cbaccf6c3c34f9
MichaelLin13 <aa0912152967>
2020-09-30 15:09:30 +0800

    init

--- app/src/main/java/com/tw/practice/MainActivity.java
+++ app/src/main/java/com/tw/practice/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int kk;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
